1f2c5efe424ca939727eb5410202d995fbc43c95
cairlinn <cairlinn@gmail.com>
2019-10-15 12:03:41 -0400

    orginit.sh clean up for snapshot

--- orginit.sh
+++ orginit.sh
@@ -2,11 +2,8 @@
 
 sfdx force:org:create -f config/project-scratch-def.json -a ADK --setdefaultusername -d 1
 
-sfdx force:source:push 
-
-#sfdx assign permission sets
-
-#sfdx 
+#not needed since using scratch org snapshot
+#sfdx force:source:push 
 
 sfdx force:apex:execute -f config/create-demo-data-setup.apex
 
