1ef25a7187b5416b0d258d95c31758c52ce5e508
cairlinn <cairlinn@gmail.com>
2019-10-17 09:52:31 -0400

    add wait time to orgInit.sh

--- orginit.sh
+++ orginit.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
 # using scratch org snapshot
-sfdx force:org:create -f config/project-scratch-def.json -a ADK --setdefaultusername -d 1
+sfdx force:org:create -f config/project-scratch-def.json -a ADK --setdefaultusername -d 1 -w 10
+
 #not needed since using scratch org snapshot
 #sfdx force:source:push 
 
