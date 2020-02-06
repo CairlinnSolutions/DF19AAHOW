db3550b4daf251553981f385875ec2d48ce68b5a
Peter Martin <peter.martin@pmartin-ltm.internal.salesforce.com>
2020-02-06 13:28:46 -0500

    redo license load. got rid of DF19 refs

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # create scratch org using regular definition file
 sfdx force:org:create -f config/project-scratch-def.json -a aademo -d 5 -w 10
 # deploy the basic configuration
@@ -7,15 +6,15 @@ sfdx force:source:deploy -m "CustomObject, PermissionSet, CustomApplication, Cus
 # assign EA admin to admin user
 sfdx force:user:permset:assign -n EinsteinAnalyticsPlusAdmin -u aademo
 # assign DF19 LMA permset to admin user
-sfdx force:user:permset:assign -n DF19_LMA -u aademo
+sfdx force:user:permset:assign -n LMA -u aademo
 # assign DF19 LMA permset to EA integration user using anonymous apex
 sfdx force:apex:execute -f ./config/assign-lma-permset.apex -u aademo
 # deploy EA components
 sfdx force:source:deploy -m "WaveDataflow, WaveDataset, WaveLens, WaveApplication, WaveDashboard" -u aademo
-# load sample accounts
-sfdx force:data:tree:import -p data/account-Account-plan.json -u aademo
-# load packages and package versions
+# load sample data
+#sfdx force:data:tree:import -p data/account-Account-plan.json -u aademo
 sfdx force:data:tree:import -p data/version-Package__c-Package_Version__c-plan.json -u aademo
+sfdx force:data:tree:import -p data/license-Account-License__c-plan.json -u aademo
 
 # run apex to send email to reset password
 #sfdx force:apex:execute -f config/create-demo-data-setup.apex -u aademo
